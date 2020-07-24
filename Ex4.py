b8ca215b07ef67d3f452ede658020d2159a272de
Zsadegh80 <68478476+Zsadegh80@users.noreply.github.com>
2020-07-24 23:19:09 +0430

    Add files via upload

---
+++ Ex4.py
@@ -0,0 +1,62 @@
+"""
+a1=int(input('enter a number :'))
+b1=int(input('enter a number :'))
+if(b1>a1):
+    print(b1)
+    print(a1)
+else:
+    print(a1)
+    print(b1)
+"""
+"""
+i=int(input('enter a that is under 20: '))
+if(i>=20):
+    print('Too high')
+else:
+    print('Thank you')
+"""
+"""
+NUM=int(input('please enter  number between 10 and 20: '))
+if(10<NUM<20):
+    print('Thank you')
+else:
+    print('Incorrect answer')
+"""
+"""
+colour=input('enter their favourit colour : ')
+if(colour=='RED' or colour=='Red'):
+    print('I like red too')
+else:
+    print('I dont like ' ,(colour) ,' ,I prefer red' )
+"""
+"""
+AGE=int(input('How old are you: '))
+if(AGE>=18):
+    print('You can vote')
+elif(AGE==17):
+    print('You can learn to drive')
+elif(AGE==16):
+    print('You lottery ticket')
+elif(AGE<16):
+    print('You can go Trick or-Treating')
+"""
+"""
+add=int(input('enter a number: '))
+if(add<10):
+    print('Too low')
+elif(10<=add<20):
+    print('Correct')
+else:
+    print('Too high')
+"""
+"""
+ADD=int(input('enter 1,2 or 3 :'))
+if(ADD==1):
+    print('Thank you')
+elif(ADD==2):
+    print('Well done')
+elif(ADD==3):
+    print('Correct')
+else:
+    print('Error massege')
+"""
